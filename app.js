64940c9aac1a00c164b40fb0fea8b6a7fd3b47b4
Younes Lahouiti <loller763@gmail.com>
2022-08-01 14:39:15 +0200

    1er commit

---
+++ app.js
@@ -0,0 +1,107 @@
+//Could've used TypeScript here but it's way too capricious
+
+const firstInputColor = document.querySelector(
+  ".container__primary-color-input"
+);
+
+const labelFirstInputColor = document.querySelector("label[for=primary-color]");
+labelFirstInputColor.style.color = "black";
+firstInputColor.style.backgroundColor = "#FF5F6D";
+
+const secondInputColor = document.querySelector(
+  ".container__secondary-color-input"
+);
+const labelSecondInputColor = document.querySelector(
+  "label[for=secondary-color]"
+);
+
+const gradientOrientationBar = document.querySelector(
+  ".container__gradient-orientation-input"
+);
+
+labelSecondInputColor.style.color = "black";
+secondInputColor.style.backgroundColor = "#FFC371";
+
+const mainPageBackgroundGradient = document.querySelector(".main--gradient");
+
+const copyButton = document.querySelector(".container__copy-button");
+
+const randomButton = document.querySelector(".container__random-color-button");
+
+const rotationSpan = document.querySelector(
+  ".container__gradient-orientation-degrees"
+);
+
+let mainPageColorPrimary = "#FF5F6D";
+let mainPageColorSecondary = "#FFC371";
+let mainPageColorOrientation = 90;
+let valueOfLinearGradient = `linear-gradient(${mainPageColorOrientation}, ${mainPageColorPrimary}, ${mainPageColorSecondary})`;
+
+firstInputColor.addEventListener("input", (e) => {
+  let inputtedColor = e.target.value;
+  labelFirstInputColor.textContent = `${inputtedColor.toUpperCase()}`;
+
+  //To make sure that the label remains visible when picking a color with a low contrast ratio
+  if (inputtedColor > "#BABABA") {
+    labelFirstInputColor.style.color = "black";
+    firstInputColor.style.border = "2px solid black";
+  } else {
+    labelFirstInputColor.style.color = "white";
+    firstInputColor.style.border = "2px solid white";
+  }
+  firstInputColor.style.backgroundColor = `${inputtedColor}`;
+  mainPageColorPrimary = inputtedColor;
+  changeMainPageBackground(
+    mainPageColorOrientation,
+    inputtedColor,
+    mainPageColorSecondary
+  );
+});
+
+secondInputColor.addEventListener("input", (e) => {
+  let inputtedColor = e.target.value;
+  labelSecondInputColor.textContent = `${inputtedColor.toUpperCase()}`;
+
+  //To make sure that the label remains visible when picking a color with a low contrast ratio
+  if (inputtedColor > "#BABABA") {
+    labelSecondInputColor.style.color = "black";
+    secondInputColor.style.border = "2px solid black";
+  } else {
+    labelSecondInputColor.style.color = "white";
+    secondInputColor.style.border = "2px solid white";
+  }
+  secondInputColor.style.backgroundColor = `${inputtedColor}`;
+  mainPageColorSecondary = inputtedColor;
+  changeMainPageBackground(
+    mainPageColorOrientation,
+    mainPageColorPrimary,
+    inputtedColor
+  );
+});
+
+gradientOrientationBar.addEventListener("input", (e) => {
+  let inputtedOrientation = e.target.value;
+  rotationSpan.textContent = `${inputtedOrientation}°`;
+  mainPageColorOrientation = inputtedOrientation;
+  changeMainPageBackground(
+    inputtedOrientation,
+    mainPageColorPrimary,
+    mainPageColorSecondary
+  );
+});
+
+function changeMainPageBackground(orientation, firstColor, secondColor) {
+  mainPageBackgroundGradient.style.backgroundImage = `linear-gradient(${orientation}deg, ${firstColor}, ${secondColor})`;
+  valueOfLinearGradient = mainPageBackgroundGradient.style.backgroundImage;
+}
+
+copyButton.addEventListener("click", () => {
+  navigator.clipboard.writeText(valueOfLinearGradient);
+  alert("Successfully copied the linear gradient to the clipboard!");
+});
+
+const colorLabels = document.querySelectorAll("label");
+
+function createRandomGradient() {}
+
+randomButton.addEventListener("click", createRandomGradient);
